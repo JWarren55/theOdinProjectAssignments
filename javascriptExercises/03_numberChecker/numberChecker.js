c396143c67bafc73b9fdabed4fab11edbce0f5ad
JWarren55 <jonwwarren55@gmail.com>
2025-10-28 10:00:40 -0500

    Add javascriptExercises
    Add jsE 03

---
+++ javascriptExercises/03_numberChecker/numberChecker.js
@@ -0,0 +1,11 @@
+
+function numberChecker(number) {
+  if (number === 6) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Do not edit below this line
+module.exports = numberChecker;
